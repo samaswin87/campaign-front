2b0fb66b121a0f7a59448e027cd414c487bf8249
samaswin87 <aswin.r312@outlook.com>
2021-05-05 17:03:43 +0530

    fixed show page issue

--- src/views/contacts/show.vue
+++ src/views/contacts/show.vue
@@ -6,116 +6,116 @@
                     <div slot="header" class="clearfix">
                         <span>Contact</span>
                     </div>
-                    <div class="content">
+                    <div>
                         <el-row class="content-row">
-                            <el-col :span="10">
+                            <el-col :span="10" class="content-label">
                                 <div class="grid-content"><label class="label-space">{{$t('table.status')}}:</label></div>
                             </el-col>
-                            <el-col :span="2">
+                            <el-col :span="10" class="content-vlaue">
                                 <div class="grid-content"><span>{{contactData.status}}</span></div>
                             </el-col>
                         </el-row>
                         <el-row class="content-row">
-                            <el-col :span="10">
+                            <el-col :span="10" class="content-label">
                                 <div class="grid-content"><label class="label-space">{{$t('table.company')}}:</label></div>
                             </el-col>
-                            <el-col :span="2">
+                            <el-col :span="10" class="content-vlaue">
                                 <div class="grid-content"><span>{{contactData.company}}</span></div>
                             </el-col>
                         </el-row>
 
                         <el-row class="content-row">
-                            <el-col :span="10">
+                            <el-col :span="10" class="content-label">
                                 <div class="grid-content"><label class="label-space">{{$t('table.contact.firstName')}}: </label></div>
                             </el-col>
-                            <el-col :span="2">
+                            <el-col :span="10" class="content-vlaue">
                                 <div class="grid-content"><span>{{contactData.first}}</span></div>
                             </el-col>
                         </el-row>
 
                         <el-row class="content-row">
-                            <el-col :span="10">
+                            <el-col :span="10" class="content-label">
                                 <div class="grid-content"><label class="label-space">{{$t('table.contact.lastName')}}: </label></div>
                             </el-col>
-                            <el-col :span="2">
+                            <el-col :span="10" class="content-vlaue">
                                 <div class="grid-content"><span>{{contactData.last}}</span></div>
                             </el-col>
                         </el-row>
 
                         <el-row class="content-row">
-                            <el-col :span="10">
+                            <el-col :span="10" class="content-label">
                                 <div class="grid-content"><label class="label-space">{{$t('table.contact.middleName')}}: </label></div>
                             </el-col>
-                            <el-col :span="2">
+                            <el-col :span="10" class="content-vlaue">
                                 <div class="grid-content"><span>{{contactData.middle}}</span></div>
                             </el-col>
                         </el-row>
 
                         <el-row class="content-row">
-                            <el-col :span="10">
+                            <el-col :span="10" class="content-label">
                                 <div class="grid-content"><label class="label-space">{{$t('table.contact.gender')}}: </label></div>
                             </el-col>
-                            <el-col :span="2">
+                            <el-col :span="10" class="content-vlaue">
                                 <div class="grid-content"><span>{{contactData.gender}}</span></div>
                             </el-col>
                         </el-row>
 
                         <el-row class="content-row">
-                            <el-col :span="10">
+                            <el-col :span="10" class="content-label">
                                 <div class="grid-content"><label class="label-space">{{$t('table.contact.email')}}: </label></div>
                             </el-col>
-                            <el-col :span="2">
+                            <el-col :span="10" class="content-vlaue">
                                 <div class="grid-content"><span>{{contactData.email}}</span></div>
                             </el-col>
                         </el-row>
 
                         <el-row class="content-row">
-                            <el-col :span="10">
+                            <el-col :span="10" class="content-label">
                                 <div class="grid-content"><label class="label-space">{{$t('table.contact.tags')}}: </label></div>
                             </el-col>
-                            <el-col :span="2">
+                            <el-col :span="10" class="content-vlaue">
                                 <div class="grid-content"><span>{{contactData.tags}}</span></div>
                             </el-col>
                         </el-row>
 
                         <el-row class="content-row">
-                            <el-col :span="10">
+                            <el-col :span="10" class="content-label">
                                 <div class="grid-content"><label class="label-space">{{$t('table.contact.noOfCampaigns')}}: </label></div>
                             </el-col>
-                            <el-col :span="2">
+                            <el-col :span="10" class="content-vlaue">
                                 <div class="grid-content"><span>{{contactData.no}}</span></div>
                             </el-col>
                         </el-row>
 
                         <el-row class="content-row">
-                            <el-col :span="10">
+                            <el-col :span="10" class="content-label">
                                 <div class="grid-content"><label class="label-space">{{$t('table.contact.keywords')}}: </label></div>
                             </el-col>
-                            <el-col :span="2">
+                            <el-col :span="10" class="content-vlaue">
                                 <div class="grid-content"><span>{{contactData.keywords}}</span></div>
                             </el-col>
                         </el-row>
 
                         <el-row class="content-row">
-                            <el-col :span="10">
+                            <el-col :span="10" class="content-label">
                                 <div class="grid-content"><label class="label-space">{{$t('table.contact.phone')}}: </label></div>
                             </el-col>
-                            <el-col :span="2">
+                            <el-col :span="10" class="content-vlaue">
                                 <div class="grid-content"><span>{{contactData.phone}}</span></div>
                             </el-col>
                         </el-row>
 
                         <el-row class="content-row">
-                            <el-col :span="10">
+                            <el-col :span="10" class="content-label">
                                 <div class="grid-content"><label class="label-space">{{$t('table.contact.notes')}}: </label></div>
                             </el-col>
-                            <el-col :span="2">
+                            <el-col :span="10" class="content-vlaue">
                                 <div class="grid-content"><span>{{contactData.notes}}</span></div>
                             </el-col>
                         </el-row>
 
                         <el-row class="content-row">
-                            <el-col :span="12">
+                            <el-col :span="12"  class="content-label">
                                 <div class="grid-content">
                                     <router-link :to="'/contacts'">
                                         <el-button type="primary" icon="el-icon-caret-left">
@@ -159,8 +159,12 @@ export default class extends Vue {
 </script>
 
 <style lang="scss" scoped>
-.content {
+.content-label {
     text-align: end;
+}
+
+.content-vlaue {
+    margin-left: 3%;
 }
 
 .content-row {
