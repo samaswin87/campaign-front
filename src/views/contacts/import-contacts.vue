557afce40fe4b42174de5898a578fb1d0ca53597
samaswin87 <aswin.r312@outlook.com>
2021-05-16 18:51:03 +0530

    added column dropdown in excel parser page for field mapping

--- src/views/contacts/import-contacts.vue
+++ src/views/contacts/import-contacts.vue
@@ -3,36 +3,61 @@
     <upload-excel-component
       :on-success="handleSuccess"
       :before-upload="beforeUpload"
+      class="margin-bottom-20"
     />
-    <el-table
-      :data="tableData"
-      border
-      highlight-current-row
-      style="width: 100%;margin-top:20px;"
-    >
-      <el-table-column
-        v-for="item of tableHeader"
-        :key="item"
-        :prop="item"
-        :label="item"
-      />
-    </el-table>
+    <el-row>
+        <el-col :span="24 / tableHeader.length"
+          v-for="item of tableHeader"
+          :key="item"
+          class="margin-bottom-20">
+          <multiselect
+            v-model="columnsMapping"
+            tag-placeholder="Add this as new column"
+            placeholder="Search or add a column"
+            :options="contactColumns"
+            :multiple="true"
+            :clear-on-select="false"
+            :close-on-select="false"
+            :taggable="true"
+            @tag="addTag"
+            >
+          </multiselect>
+        </el-col>
+        <el-col :span="24">
+          <el-table
+            :data="tableData"
+            border
+            highlight-current-row
+          >
+            <el-table-column
+              v-for="item of tableHeader"
+              :key="item"
+              :prop="item"
+              :label="item"
+            />
+          </el-table>
+        </el-col>
+    </el-row>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import UploadExcelComponent from '@/components/UploadExcel/index.vue'
+import Multiselect from 'vue-multiselect'
 
 @Component({
   name: 'UploadExcel',
   components: {
-    UploadExcelComponent
+    UploadExcelComponent,
+    Multiselect
   }
 })
 export default class extends Vue {
   private tableData: any = []
   private tableHeader: string[] = []
+  private columnsMapping: string[] = []
+  private contactColumns: string[] = ['First Name', 'Last Name', 'Phone Number', 'Email', 'Middle Name', 'Gender']
 
   private beforeUpload(file: File) {
     const isLt1M = file.size / 1024 / 1024 < 1
@@ -50,5 +75,15 @@ export default class extends Vue {
     this.tableData = results
     this.tableHeader = header
   }
+
+  addTag(newColumn: string) {
+    this.contactColumns.push(newColumn)
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+::v-deep .multiselect__content-wrapper {
+  background-color: rgb(223, 228, 237);
+}
+</style>
