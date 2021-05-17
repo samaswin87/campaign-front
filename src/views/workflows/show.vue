5f9bdc51afd4c0ec5190ef4ffb522f40a27e9abf
samaswin87 <aswin.r312@outlook.com>
2021-05-17 23:29:49 +0530

    added workflow show page

--- src/views/workflows/show.vue
+++ src/views/workflows/show.vue
@@ -4,196 +4,272 @@
             <el-row>
                 <el-card class="box-card">
                     <div slot="header" class="clearfix">
-                        <span>Workflow</span>
+                        <span>Show workflow</span>
+                        <el-button type="primary" class="float-right"><svg-icon name="save" /></el-button>
                     </div>
-                    <div>
-                        <el-row>
-                            <el-col :span="6">
-                                <el-row>
-                                    <el-card class="box-card workflow-details">
-                                        <div slot="header" class="clearfix">
-                                            <el-tooltip class="item" effect="dark" content="Start/Publish workflow" placement="bottom">
-                                                <el-button type="success" size="mini" icon="el-icon-video-play"></el-button>
-                                            </el-tooltip>
-                                            <el-tooltip class="item" effect="dark" content="Edit" placement="bottom">
-                                                <el-button type="info" size="mini" icon="el-icon-edit-outline" class="right-button"></el-button>
-                                            </el-tooltip>
-                                        </div>
-                                        <div>
-                                            <div class="grid-workflow">
-                                                <el-tooltip class="item" effect="dark" content="Company" placement="right">
-                                                    <i class="el-icon-office-building icon-padding"></i>
-                                                </el-tooltip>
-                                                <span>{{workflowData.company}}</span>
-                                            </div>
-                                            <div class="grid-workflow">
-                                                <el-tooltip class="item" effect="dark" content="Workflow Type" placement="right">
-                                                    <i :class="typeIcon(workflowData.type)"></i>
-                                                </el-tooltip>
-                                                <span>{{typeName(workflowData.type)}}</span>
-                                            </div>
-                                            <div class="grid-workflow" v-if="workflowData.type === 'scheduled'">
-                                                <el-tooltip class="item" effect="dark" content="Scheduled On" placement="right">
-                                                    <i class="el-icon-timer icon-padding"></i>
-                                                </el-tooltip>
-                                                <span>{{workflowData.scheduledOn | parseTime }}</span>
-                                            </div>
-                                            <div class="grid-workflow" v-if="workflowData.type === 'recurring'">
-                                                <el-tooltip class="item" effect="dark" content="Recurring Days" placement="right">
-                                                    <i class="el-icon-date icon-padding"></i>
-                                                </el-tooltip>
-                                                <span class="svg-container" v-for="day in recurringDays" :key="day">
-                                                    <svg-icon :name="dayIcon(day)" :class="dayIconEnable(day, workflowData.recurringDays)"/>
-                                                </span>
-                                            </div>
-                                            <div class="grid-workflow" v-if="workflowData.type === 'recurring'">
-                                                <el-tooltip class="item" effect="dark" content="Recurring At" placement="right">
-                                                    <i class="el-icon-timer icon-padding"></i>
-                                                </el-tooltip>
-                                                <span>{{workflowData.recurringAt | parseTime('{h}:{i}:{s}') }}</span>
-                                            </div>
-                                            <div class="grid-workflow">
-                                                <el-tooltip class="item" effect="dark" content="Contact count" placement="right">
-                                                    <i class="el-icon-s-data icon-padding"></i>
-                                                </el-tooltip>
-                                                <span>{{workflowData.noOfContacts}}</span>
-                                            </div>
-                                            <div class="grid-workflow">
-                                                <el-tooltip class="item" effect="dark" content="Platform Number" placement="right">
-                                                    <i class="el-icon-phone icon-padding"></i>
-                                                </el-tooltip>
-                                                <span>{{workflowData.phone}}</span>
-                                            </div>
-                                        </div>
-                                    </el-card>
-                                </el-row>
-                                <el-row>
-                                    <el-card class="box-card workflow-details">
-                                        <div slot="header" class="clearfix">
-                                            <i class="el-icon-message icon-padding"></i>
-                                            <span>Message</span>
-                                        </div>
-                                        <div>
-                                            {{workflowData.body}}
-                                        </div>
-                                    </el-card>
-                                </el-row>
-                            </el-col>
-                            <el-col :span="18">
-                                <el-card class="box-card workflow-recipients">
-                                    <RecipientsTable />
+                    <el-row>
+                        <el-col :span="8">
+                            <el-row>
+                                <el-card class="box-card workflow-details">
+                                    <el-form
+                                    ref="workflowForm"
+                                    :model="workflowData"
+                                    class="form-container"
+                                    >
+                                        <el-row class="content-row">
+                                            <el-col :span="2">
+                                                <svg-icon name="company" class="svg-icon-botton"/>
+                                            </el-col>
+                                            <el-col :span="20">
+                                                <div class="grid-value">
+                                                    <el-input
+                                                    v-model="workflowData.company"
+                                                    disabled>
+                                                    </el-input>
+                                                </div>
+                                            </el-col>
+                                        </el-row>
+
+                                        <el-row class="content-row">
+                                            <el-col :span="2">
+                                                <svg-icon name="keyword" class="svg-icon-botton"/>
+                                            </el-col>
+                                            <el-col :span="20">
+                                                <div class="grid-value">
+                                                    <span>
+                                                        <el-input
+                                                        v-model="workflowData.name"
+                                                        disabled>
+                                                        </el-input>
+                                                    </span>
+                                                </div>
+                                            </el-col>
+                                        </el-row>
+
+                                        <el-row class="content-row">
+                                            <el-col :span="2">
+                                                <svg-icon name="phone" class="svg-icon-botton"/>
+                                            </el-col>
+                                            <el-col :span="20">
+                                                <div class="grid-value">
+                                                    <span>
+                                                        <el-input
+                                                        v-model="workflowData.phone"
+                                                        disabled>
+                                                        </el-input>
+                                                    </span>
+                                                </div>
+                                            </el-col>
+                                        </el-row>
+
+                                        <el-row class="content-row">
+                                            <el-col :span="20" class="content-vlaue">
+                                                <div class="grid-content">
+                                                    <span>
+                                                        <el-switch
+                                                        v-model="workflowData.confidential"
+                                                        active-text="Confidential"
+                                                        active-color="#ff4949"
+                                                        inactive-color="#13ce66"
+                                                        inactive-text="Non Confidential"
+                                                        disabled
+                                                        >
+                                                        </el-switch>
+                                                    </span>
+                                                </div>
+                                            </el-col>
+                                        </el-row>
+                                    </el-form>
                                 </el-card>
-                            </el-col>
-                        </el-row>
-                    </div>
+                            </el-row>
+                        </el-col>
+                        <el-col :span="16">
+                            <el-card class="box-card workflow-promt content-card">
+                                <div slot="header" class="clearfix">
+                                    <el-tooltip class="item" effect="dark" content="Add Promts" placement="bottom">
+                                        <el-button type="primary" icon="el-icon-circle-plus" @click="addPromt"></el-button>
+                                    </el-tooltip>
+                                    <span class="content-space">Prompts are sent in the order they appear.</span>
+                                </div>
+                                <draggable
+                                    :list="promtList"
+                                    :key.sync="draggableList"
+                                    class="dragArea"
+                                    @end="updateItemOrder($event)"
+                                >
+                                    <div
+                                    v-for="element in promtList"
+                                    :key="element.order"
+                                    class="list-complete-item"
+                                    >
+                                        <el-card shadow="hover" class="all-scroll">
+                                            <svg-icon
+                                            class="margin-right-10"
+                                            name="drag"
+                                            width="20"
+                                            height="20"
+                                            />
+                                            <span>{{ element.body }} Order: [{{element.order}}]</span>
+                                            <i class="el-icon-delete-solid float-right pointer" @click="removePromt(element)"/>
+                                        </el-card>
+                                    </div>
+                                </draggable>
+                            </el-card>
+                                <el-card class="box-card workflow-promt">
+                                    <div slot="header" class="clearfix">
+                                        <el-button
+                                            type="primary"
+                                            @click="addDestination"
+                                        >
+                                            <svg-icon name="add-url" />
+                                        </el-button>
+                                        <span class="content-space">Final Response</span>
+                                    </div>
+                                    <span v-html="displayResponse(finalResponseData.body)"></span>
+                                </el-card>
+                        </el-col>
+                    </el-row>
                 </el-card>
             </el-row>
+
+            <AddPromt
+            :visible.sync="promtLoading"
+            :order.sync="order"
+            @promtRecord="addPromtRecord"
+            />
+
+            <AddDestinationURL
+            :visible.sync="destinationLoading"
+            @addDestinationURL="addDestinationURL"
+            />
         </div>
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import { defaultWorkflowData, getWorkflow } from '@/api/workflows'
-import RecipientsTable from '@/views/recipients/index.vue'
+import { defaultWorkflowData, defaultFinalResponseData, getWorkflow } from '@/api/workflows'
+import { getCompanies } from '@/api/companies'
+import Multiselect from 'vue-multiselect'
+import { map } from 'lodash'
+import AddPromt from './components/AddPromt.vue'
+import AddDestinationURL from './components/AddDestinationURL.vue'
+import Draggable from 'vuedraggable'
 
 @Component({
-  name: 'WorkflowView',
+  name: 'WorkflowCreate',
   components: {
-    RecipientsTable
+    Multiselect,
+    AddPromt,
+    Draggable,
+    AddDestinationURL
   }
 })
 export default class extends Vue {
     private workflowData = defaultWorkflowData
-    private recurringDays :string[] = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    private finalResponseData = defaultFinalResponseData
+    private companies :string[] = []
+    private promtLoading = false
+    private destinationLoading = false
+    private promtList:any[] = []
+    private draggableList = 0
+    private order = 0
 
     created() {
+      this.fetchCompanies()
       const id = this.$route.params && this.$route.params.id
       this.fetchData(parseInt(id))
+    }
+
+    private async fetchCompanies() {
+      try {
+        const { data } = await getCompanies({})
+        this.companies = map(data.items, 'name')
+      } catch (err) {
+        console.error(err)
+      }
+    }
+
+    private addPromt() {
+      this.promtLoading = true
+    }
+
+    private addDestination() {
+      this.destinationLoading = true
+    }
+
+    private addPromtRecord(data: any) {
+      this.promtList.push(data)
+      this.order = this.promtList.length
+      this.draggableList += 1
+    }
+
+    private removePromt(element: any) {
+      const index = this.promtList.indexOf(element)
+      this.promtList.splice(index, 1)
+      this.order = this.promtList.length
+    }
+
+    private updateItemOrder(event: any) {
+      console.log('Old Index: ' + event.oldIndex)
+      console.log('New Index: ' + event.newIndex)
+    }
+
+    private addDestinationURL(data: any) {
+      console.log('Add destination URL to object :' + data)
     }
 
     private async fetchData(id: number) {
       try {
         const { data } = await getWorkflow(id, { /* Your params here */ })
         this.workflowData = data.workflow
+        this.finalResponseData = data.workflow.finalResponse
+        this.promtList = data.workflow.promts
+        this.order = this.promtList.length
       } catch (err) {
         console.error(err)
       }
     }
 
-    private typeIcon(type: string) {
-      const typeMap: { [key: string]: string } = {
-        scheduled: 'el-icon-time icon-padding',
-        immediate: 'el-icon-s-promotion icon-padding',
-        recurring: 'el-icon-date icon-padding',
-        one_by_one: 'el-icon-s-comment icon-padding'
-      }
-      return typeMap[type]
-    }
-
-    private typeName(type: string) {
-      const typeMap: { [key: string]: string } = {
-        scheduled: 'Schedule: Pick a date and time to send',
-        immediate: 'Immediate: Send all messages now',
-        recurring: 'Recurring: Send a message over and over',
-        one_by_one: 'One-by-One: Send each message manually'
-      }
-      return typeMap[type]
-    }
-
-    private dayIcon(day: string) {
-      const dayMap: { [key: string]: string } = {
-        Mon: 'Monday',
-        Tue: 'Tuesday',
-        Wed: 'Wednesday',
-        Thu: 'Thursday',
-        Fri: 'Friday',
-        Sat: 'Saturday',
-        Sun: 'Sunday'
-      }
-      return dayMap[day]
-    }
-
-    private dayIconEnable(day: any, recurringDays: string[]) {
-      return recurringDays.includes(day) ? 'svg-padding svg-color' : 'svg-padding'
+    private displayResponse(body: string) {
+      return body.replace('{destinationURL}', '<span class="el-tag el-tag--danger el-tag--medium el-tag--light">{destinationURL}</span>')
     }
 }
 </script>
 
 <style lang="scss" scoped>
-.tags {
-  margin-left: 6px;
+
+.content-card {
+    margin-bottom: 2%;
 }
 
-.workflow-details {
-    margin-right: 1%;
+.content-row {
+    margin-bottom: 5%;
 }
 
-.icon-padding {
-    padding-right: 10px
+.content-space {
+    margin-left: 10px;
 }
 
-.right-button {
-    float: right;
+.content-button {
+    margin-left: 28px;
 }
 
-.grid-workflow {
-    margin-bottom: 10px;
+.content-vlaue {
+    padding-left: 50px;
 }
 
-.svg-container {
-    padding: 6px 5px 6px 0px;
-    vertical-align: middle;
-    width: 40px;
-    display: inline-block;
-  }
-
-.svg-color {
-    color: #42b983;
+.svg-icon-botton {
+    width: 30px !important;
+    height: 30px !important;
+    margin-top: 8px;
 }
 
-.svg-padding {
-    width: 2.5em !important;
-    height: 2.5em !important;
+.all-scroll {
+    cursor: all-scroll;
+}
+
+.pointer {
+    cursor: pointer;
 }
 </style>
