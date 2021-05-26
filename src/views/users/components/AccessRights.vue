d1b6877ef8d63e0ba698511baa09c31db724bc51
samaswin87 <aswin.r312@outlook.com>
2021-05-26 22:56:42 +0530

    fixed access rights page

--- src/views/users/components/AccessRights.vue
+++ src/views/users/components/AccessRights.vue
@@ -7,15 +7,24 @@
         <div class="user-rights">
             <el-row :gutter="12" class="panel-group">
                 <el-col :xs="12" :sm="12" :lg="6" v-for="item in accessRights" :key="item.id">
-                    <el-card shadow="hover" class="mb-10-px">
+                    <el-card shadow="hover" class="mb-10-px" v-if="item.menu">
                         <span :class="item.className">
                             <el-checkbox v-model="item.moduleSelected" :label="item.moduleName" border><svg-icon :name="item.logo"></svg-icon> {{item.moduleName}}</el-checkbox>
                         </span>
-                        <span class="mt-10-px d-flex" v-if="item.menu">
+                        <span class="mt-10-px d-flex">
                             <el-checkbox v-model="item.list" label="List"><svg-icon name="list"></svg-icon></el-checkbox>
                             <el-checkbox v-model="item.show" label="Show"><svg-icon name="eye-on" ></svg-icon></el-checkbox>
                             <el-checkbox v-model="item.create" label="Create"><svg-icon name="add"></svg-icon></el-checkbox>
                             <el-checkbox v-model="item.edit" label="Edit"><svg-icon name="edit"></svg-icon></el-checkbox>
+                        </span>
+                    </el-card>
+                </el-col>
+            </el-row>
+            <el-row :gutter="12" class="panel-group">
+                <el-col :xs="12" :sm="12" :lg="6" v-for="item in accessRights" :key="item.id">
+                    <el-card shadow="hover" class="mb-10-px" v-if="!item.menu">
+                        <span :class="item.className">
+                            <el-checkbox v-model="item.moduleSelected" :label="item.moduleName" border><svg-icon :name="item.logo"></svg-icon> {{item.moduleName}}</el-checkbox>
                         </span>
                     </el-card>
                 </el-col>
