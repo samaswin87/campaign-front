c192654713d7d750425b13daa4e9d27c21b95a20
samaswin87 <aswin.r312@outlook.com>
2021-06-13 23:29:52 +0530

    fixed campaign show page api

--- src/views/campaigns/show.vue
+++ src/views/campaigns/show.vue
@@ -3,85 +3,88 @@
         <div class="campaign-container">
             <el-row>
                 <el-card class="box-card">
-                    <div slot="header" class="clearfix">
-                        <span>Campaign</span>
+                    <div slot="header">
+                        <span>Campaign - {{this.campaignData.name}}</span>
                     </div>
                     <div>
                         <el-row>
                             <el-col :span="6">
                                 <el-row>
-                                    <el-card class="box-card campaign-details">
-                                        <div slot="header" class="clearfix">
-                                            <el-tooltip class="item" effect="dark" content="Start/Publish campaign" placement="bottom">
-                                                <el-button type="success" size="mini" icon="el-icon-video-play"></el-button>
+                                    <el-card class="box-card">
+                                        <div slot="header">
+                                            <el-tooltip class="item" effect="dark" :content="stateIcon(campaignData.state).content" placement="bottom">
+                                                <el-button
+                                                    :type="stateIcon(campaignData.state).type"
+                                                    size="mini"
+                                                    :icon="stateIcon(campaignData.state).icon"
+                                                    @click="changeState">
+                                                </el-button>
                                             </el-tooltip>
                                             <el-tooltip class="item" effect="dark" content="Edit" placement="bottom">
-                                                <el-button type="info" size="mini" icon="el-icon-edit-outline" class="right-button"></el-button>
+                                                <el-button type="info" size="mini" icon="el-icon-edit-outline" class="float-right"></el-button>
                                             </el-tooltip>
                                         </div>
                                         <div>
-                                            <div class="grid-campaign">
-                                                <el-tooltip class="item" effect="dark" content="Company" placement="right">
-                                                    <i class="el-icon-office-building icon-padding"></i>
-                                                </el-tooltip>
-                                                <span>{{campaignData.company}}</span>
-                                            </div>
-                                            <div class="grid-campaign">
-                                                <el-tooltip class="item" effect="dark" content="Campaign Type" placement="right">
-                                                    <i :class="typeIcon(campaignData.type)"></i>
-                                                </el-tooltip>
-                                                <span>{{typeName(campaignData.type)}}</span>
-                                            </div>
-                                            <div class="grid-campaign" v-if="campaignData.type === 'scheduled'">
-                                                <el-tooltip class="item" effect="dark" content="Scheduled On" placement="right">
-                                                    <i class="el-icon-timer icon-padding"></i>
-                                                </el-tooltip>
-                                                <span>{{campaignData.scheduledOn | parseTime }}</span>
-                                            </div>
-                                            <div class="grid-campaign" v-if="campaignData.type === 'recurring'">
-                                                <el-tooltip class="item" effect="dark" content="Recurring Days" placement="right">
-                                                    <i class="el-icon-date icon-padding"></i>
-                                                </el-tooltip>
-                                                <span class="svg-container" v-for="day in recurringDays" :key="day">
-                                                    <svg-icon :name="dayIcon(day)" :class="dayIconEnable(day, campaignData.recurringDays)"/>
-                                                </span>
-                                            </div>
-                                            <div class="grid-campaign" v-if="campaignData.type === 'recurring'">
-                                                <el-tooltip class="item" effect="dark" content="Recurring At" placement="right">
-                                                    <i class="el-icon-timer icon-padding"></i>
-                                                </el-tooltip>
-                                                <span>{{campaignData.recurringAt | parseTime('{h}:{i}:{s}') }}</span>
-                                            </div>
-                                            <div class="grid-campaign">
-                                                <el-tooltip class="item" effect="dark" content="Contact count" placement="right">
-                                                    <i class="el-icon-s-data icon-padding"></i>
-                                                </el-tooltip>
-                                                <span>{{campaignData.noOfContacts}}</span>
-                                            </div>
-                                            <div class="grid-campaign">
-                                                <el-tooltip class="item" effect="dark" content="Platform Number" placement="right">
-                                                    <i class="el-icon-phone icon-padding"></i>
-                                                </el-tooltip>
-                                                <span>{{campaignData.phone}}</span>
-                                            </div>
+                                            <el-tooltip class="item" effect="dark" content="Company" placement="right">
+                                                <i class="el-icon-office-building mr-10-px mb-10-px"></i>
+                                            </el-tooltip>
+                                            <span>{{campaignData.companyName}}</span>
+                                        </div>
+                                        <div>
+                                            <el-tooltip class="item" effect="dark" content="Campaign Type" placement="right">
+                                                <i :class="typeIcon(campaignData.group)"></i>
+                                            </el-tooltip>
+                                            <span>{{typeName(campaignData.group)}}</span>
+                                        </div>
+                                        <div v-if="campaignData.type === 'scheduled'">
+                                            <el-tooltip class="item" effect="dark" content="Scheduled On" placement="right">
+                                                <i class="el-icon-timer mr-10-px mb-10-px"></i>
+                                            </el-tooltip>
+                                            <span>{{campaignData.scheduledOn | parseTime }}</span>
+                                        </div>
+                                        <div v-if="campaignData.type === 'recurring'">
+                                            <el-tooltip class="item" effect="dark" content="Recurring Days" placement="right">
+                                                <i class="el-icon-date mr-10-px mb-10-px"></i>
+                                            </el-tooltip>
+                                            <span class="svg-container" v-for="day in recurringDays" :key="day">
+                                                <svg-icon :name="dayIcon(day)" :class="dayIconEnable(day, campaignData.recurringDays)"/>
+                                            </span>
+                                        </div>
+                                        <div v-if="campaignData.type === 'recurring'">
+                                            <el-tooltip class="item" effect="dark" content="Recurring At" placement="right">
+                                                <i class="el-icon-timer mr-10-px mb-10-px"></i>
+                                            </el-tooltip>
+                                            <span>{{campaignData.recurringAt | parseTime('{h}:{i}:{s}') }}</span>
+                                        </div>
+                                        <div>
+                                            <el-tooltip class="item" effect="dark" content="Contact count" placement="right">
+                                                <i class="el-icon-s-data mr-10-px mb-10-px"></i>
+                                            </el-tooltip>
+                                            <span>{{campaignData.noOfContacts}}</span>
+                                        </div>
+                                        <div>
+                                            <el-tooltip class="item" effect="dark" content="Platform Number" placement="right">
+                                                <i class="el-icon-phone mr-10-px mb-10-px"></i>
+                                            </el-tooltip>
+                                            <span>{{campaignData.operator}}</span>
                                         </div>
                                     </el-card>
                                 </el-row>
                                 <el-row>
                                     <el-card class="box-card campaign-details">
-                                        <div slot="header" class="clearfix">
-                                            <i class="el-icon-message icon-padding"></i>
+                                        <div slot="header">
+                                            <i class="el-icon-message mr-10-px"></i>
                                             <span>Message</span>
                                         </div>
                                         <div>
-                                            {{campaignData.body}}
+                                            {{campaignData.message}}
                                         </div>
                                     </el-card>
                                 </el-row>
                             </el-col>
                             <el-col :span="18">
                                 <el-card class="box-card campaign-recipients">
-                                    <RecipientsTable />
+                                    <!-- <RecipientsTable /> -->
                                 </el-card>
                             </el-col>
                         </el-row>
@@ -94,8 +97,9 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import { defaultCampaignData, getCampaign } from '@/api/campaigns'
+import { defaultCampaignData, getCampaign, updateCampaign } from '@/api/campaigns'
 import RecipientsTable from '@/views/recipients/index.vue'
+import { convertToJSON } from '@/utils/json'
 
 @Component({
   name: 'CampaignView',
@@ -104,7 +108,7 @@ import RecipientsTable from '@/views/recipients/index.vue'
   }
 })
 export default class extends Vue {
-    private campaignData = defaultCampaignData
+    private campaignData:any = defaultCampaignData
     private recurringDays :string[] = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
     created() {
@@ -114,8 +118,8 @@ export default class extends Vue {
 
     private async fetchData(id: number) {
       try {
-        const { data } = await getCampaign(id, { /* Your params here */ })
-        this.campaignData = data.campaign
+        const { data } = await getCampaign(id, {})
+        this.campaignData = convertToJSON(data)
       } catch (err) {
         console.error(err)
       }
@@ -123,10 +127,10 @@ export default class extends Vue {
 
     private typeIcon(type: string) {
       const typeMap: { [key: string]: string } = {
-        scheduled: 'el-icon-time icon-padding',
-        immediate: 'el-icon-s-promotion icon-padding',
-        recurring: 'el-icon-date icon-padding',
-        one_by_one: 'el-icon-s-comment icon-padding'
+        scheduled: 'el-icon-time mr-10-px mb-10-px',
+        immediate: 'el-icon-s-promotion mr-10-px mb-10-px',
+        recurring: 'el-icon-date mr-10-px mb-10-px',
+        one_by_one: 'el-icon-s-comment mr-10-px mb-10-px'
       }
       return typeMap[type]
     }
@@ -154,46 +158,25 @@ export default class extends Vue {
       return dayMap[day]
     }
 
+    private stateIcon(state: string) {
+      const stateMap: { [key: string]: any } = {
+        draft: { icon: 'el-icon-video-play', type: 'info', content: 'Start/Publish campaign' },
+        published: { icon: 'el-icon-video-play', type: 'success', content: 'Stop/Unpublish campaign' },
+        unpublished: { icon: 'el-icon-video-pause', type: 'danger', content: 'Start/Publish campaign again' }
+      }
+      return stateMap[state]
+    }
+
     private dayIconEnable(day: any, recurringDays: string[]) {
       return recurringDays.includes(day) ? 'svg-padding svg-color' : 'svg-padding'
     }
+
+    private async changeState() {
+      try {
+        const { data } = await updateCampaign(this.campaignData.id, {})
+      } catch (err) {
+        console.error(err)
+      }
+    }
 }
 </script>
-
-<style lang="scss" scoped>
-.tags {
-  margin-left: 6px;
-}
-
-.campaign-details {
-    margin-right: 1%;
-}
-
-.icon-padding {
-    padding-right: 10px
-}
-
-.right-button {
-    float: right;
-}
-
-.grid-campaign {
-    margin-bottom: 10px;
-}
-
-.svg-container {
-    padding: 6px 5px 6px 0px;
-    vertical-align: middle;
-    width: 40px;
-    display: inline-block;
-  }
-
-.svg-color {
-    color: #42b983;
-}
-
-.svg-padding {
-    width: 2.5em !important;
-    height: 2.5em !important;
-}
-</style>
